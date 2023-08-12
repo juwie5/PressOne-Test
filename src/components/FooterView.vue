9969ad1eba693dfbe8d7bf88f615f95b03abd817
Adejuwon Oshadipe <bibire95@gmail.com>
2023-08-12 22:39:24 +0100

    Feat: Footer added

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,13 +1,212 @@
 <template>
-    <div>
-
+  <section class="footer">
+    <div class="footer-section">
+      <div class="footer-set">
+        <PressOneLogo />
+        <p>hello@pressone.africa</p>
+        <p>+234 1 343 8962</p>
+        <div class="footer-icons">
+          <svg
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="facebook">
+              <path
+                id="Vector"
+                d="M18 2.33374H15C13.6739 2.33374 12.4021 2.86052 11.4645 3.79821C10.5268 4.73589 10 6.00766 10 7.33374V10.3337H7V14.3337H10V22.3337H14V14.3337H17L18 10.3337H14V7.33374C14 7.06852 14.1054 6.81417 14.2929 6.62663C14.4804 6.4391 14.7348 6.33374 15 6.33374H18V2.33374Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          <svg
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="twitter">
+              <path
+                id="Vector"
+                d="M23 3.33379C22.0424 4.00926 20.9821 4.5259 19.86 4.86379C19.2577 4.1713 18.4573 3.68048 17.567 3.45771C16.6767 3.23495 15.7395 3.29098 14.8821 3.61824C14.0247 3.9455 13.2884 4.52819 12.773 5.2875C12.2575 6.04682 11.9877 6.94612 12 7.86379V8.86379C10.2426 8.90935 8.50127 8.5196 6.93101 7.72923C5.36074 6.93887 4.01032 5.77242 3 4.33379C3 4.33379 -1 13.3338 8 17.3338C5.94053 18.7318 3.48716 19.4327 1 19.3338C10 24.3338 21 19.3338 21 7.83379C20.9991 7.55524 20.9723 7.27738 20.92 7.00379C21.9406 5.99728 22.6608 4.7265 23 3.33379V3.33379Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          <svg
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="instagram">
+              <path
+                id="Vector"
+                d="M17 2.33374H7C4.23858 2.33374 2 4.57232 2 7.33374V17.3337C2 20.0952 4.23858 22.3337 7 22.3337H17C19.7614 22.3337 22 20.0952 22 17.3337V7.33374C22 4.57232 19.7614 2.33374 17 2.33374Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                id="Vector_2"
+                d="M16 11.7038C16.1234 12.536 15.9813 13.386 15.5938 14.1328C15.2063 14.8796 14.5932 15.4852 13.8416 15.8635C13.0901 16.2417 12.2385 16.3734 11.4078 16.2397C10.5771 16.1061 9.80977 15.7139 9.21485 15.119C8.61993 14.524 8.22774 13.7567 8.09408 12.926C7.96042 12.0954 8.09208 11.2437 8.47034 10.4922C8.8486 9.74065 9.4542 9.12754 10.201 8.74004C10.9478 8.35254 11.7978 8.21039 12.63 8.3338C13.4789 8.45968 14.2649 8.85526 14.8717 9.4621C15.4785 10.0689 15.8741 10.8549 16 11.7038Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                id="Vector_3"
+                d="M17.5 6.83374H17.51"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+          <svg
+            width="24"
+            height="25"
+            viewBox="0 0 24 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="linkedin">
+              <path
+                id="Vector"
+                d="M16 8.33374C17.5913 8.33374 19.1174 8.96588 20.2426 10.0911C21.3679 11.2163 22 12.7424 22 14.3337V21.3337H18V14.3337C18 13.8033 17.7893 13.2946 17.4142 12.9195C17.0391 12.5445 16.5304 12.3337 16 12.3337C15.4696 12.3337 14.9609 12.5445 14.5858 12.9195C14.2107 13.2946 14 13.8033 14 14.3337V21.3337H10V14.3337C10 12.7424 10.6321 11.2163 11.7574 10.0911C12.8826 8.96588 14.4087 8.33374 16 8.33374V8.33374Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                id="Vector_2"
+                d="M6 9.33374H2V21.3337H6V9.33374Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                id="Vector_3"
+                d="M4 6.33374C5.10457 6.33374 6 5.43831 6 4.33374C6 3.22917 5.10457 2.33374 4 2.33374C2.89543 2.33374 2 3.22917 2 4.33374C2 5.43831 2.89543 6.33374 4 6.33374Z"
+                stroke="#F3BF24"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </svg>
+        </div>
+      </div>
+      <div class="footer-set">
+        <h6>Company</h6>
+        <p>Help & Support</p>
+        <p>About Us</p>
+        <p>Product</p>
+        <p>Media</p>
+        <p>FAQs</p>
+      </div>
+      <div class="footer-set">
+        <h6>Address</h6>
+        <p>The Lennox Mall, 3 Admiralty Way,</p>
+        <p>Lekki Phase 1, Lagos</p>
+      </div>
     </div>
+    <div class="footer-section">
+      <div class="footer-section2">
+        <div class="footer-inner">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M10 2.1875C5.69187 2.1875 2.1875 5.6925 2.1875 10C2.1875 14.3075 5.69219 17.8125 10 17.8125C14.3081 17.8125 17.8125 14.3075 17.8125 10C17.8125 5.6925 14.3078 2.1875 10 2.1875ZM10 16.2503C6.55344 16.2503 3.74969 13.4466 3.74969 10C3.74969 6.55406 6.55313 3.75031 10 3.75031C13.4462 3.75031 16.2497 6.55406 16.2497 10C16.2494 13.4466 13.4459 16.2503 10 16.2503Z"
+              fill="#474B50"
+            />
+            <path
+              d="M7.94188 7.60531C8.2313 7.2985 8.5772 7.05042 8.96063 6.87469C9.352 6.69489 9.77806 6.60315 10.2088 6.60594C10.7553 6.60594 11.2569 6.73625 11.7141 6.99687C12.1647 7.25175 12.5487 7.60962 12.8347 8.04125L13.9484 7.20844C13.5253 6.63076 12.9755 6.15763 12.3413 5.82531C11.6969 5.48344 10.9856 5.3125 10.2084 5.3125C9.56813 5.3125 8.96625 5.43625 8.40313 5.68437C7.85269 5.92242 7.35279 6.26343 6.93031 6.68906C6.50845 7.11724 6.17166 7.62158 5.93781 8.17531C5.69438 8.74219 5.57281 9.35125 5.57281 10.0003C5.57281 10.6487 5.69438 11.2572 5.93781 11.8256C6.17141 12.3791 6.50824 12.8831 6.93031 13.3106C7.35282 13.7365 7.85269 14.078 8.40313 14.3166C8.96656 14.5634 9.56844 14.6875 10.2084 14.6875C10.9856 14.6875 11.6966 14.5166 12.3413 14.1744C12.9759 13.8426 13.5258 13.3693 13.9484 12.7912L12.8347 11.9597C12.5483 12.3907 12.1644 12.7481 11.7141 13.0031C11.2569 13.2634 10.7553 13.3938 10.2088 13.3938C9.77805 13.3967 9.35195 13.3049 8.96063 13.125C8.57714 12.9494 8.23121 12.7013 7.94188 12.3944C7.65188 12.0881 7.42125 11.7284 7.25031 11.3191C7.07938 10.9097 6.99407 10.4694 6.99407 10.0003C6.99407 9.53031 7.07938 9.09062 7.25031 8.68094C7.42125 8.27125 7.65188 7.91281 7.94188 7.60531"
+              fill="#474B50"
+            />
+          </svg>
+          <p>2022 PressOne Africa</p>
+        </div>
+        <p>All rights reserved</p>
+      </div>
+      <div class="footer-section2">
+        <p>Terms & Agreement</p>
+        <p>Privacy Policy</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
-
+import PressOneLogo from "../assets/icons/PressOneLogo.vue";
 </script>
 
 <style lang="scss" scoped>
+@import "../src/styles/default.scss";
+.footer {
+  background-color: #fef8e7;
+  padding: 80px 200px;
+}
 
+.footer-section {
+  display: flex;
+  justify-content: space-between;
+}
+.footer-set {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  h6 {
+    font-size: 24px;
+    color: $black;
+    margin: 0;
+  }
+  p {
+    font-size: 18px;
+    color: $grey;
+    margin: 0;
+    font-weight: 300;
+  }
+}
+
+.footer-set:last-child {
+  align-items: flex-end;
+}
+.footer-icons {
+  display: flex;
+  gap: 24px;
+}
+
+.footer-section2{
+    display: flex;
+    gap: 24px;
+    align-items: center;
+    p{
+        color: $grey2;
+    }
+}
+.footer-inner{
+    display: flex;
+    align-items: center;
+}
 </style>
